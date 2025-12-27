64b0ea57e34799b5636da2663f83f6bb84293586
deepakumari867 <deepakumari2301@gmail.com>
2025-12-27 23:04:46 +0530

    all changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MyWardrobe from "./pages/MyWardrobe";
+import Navbar from "./components/Navbar";
+
 
 export default function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/my-wardrobe" element={<MyWardrobe />} />
+         <Route path="/my-wardrobe" element={<MyWardrobe/>} />
       </Routes>
     </Router>
   );
